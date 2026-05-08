199f9aa9d8953008e92647e157125027cac2135d
OliverBoliverTheBetter <oliver.reidkonta@education.nsw.gov.au>
2026-05-08 15:09:54 +1000

    I HAVE STARTED MAINING LAZARUS

---
+++ main.py
@@ -0,0 +1,20 @@
+#This is THE code of all time
+#Menu options: view all my data, visualise the data, search for the data, edit the data, exit the data
+#imports
+
+#functions
+def TheMain():
+    while True:
+        print("yo wassup this is the DATA you gotta believe me\n")
+        print("The choices:\n1. View all my data\n2. Visualise all my data\n3. Search for the data\n4. Edit my data\n5. Exit this lovely Juvely program.")
+        try:
+            thechoicening = int(input("\nYou  must now decide your fate.\n"))
+        except:
+            print("\nStupid dumb dumb stupid fart face idiot\n")
+        
+
+
+
+
+#The happening area
+TheMain()
