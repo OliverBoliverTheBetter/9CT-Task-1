9eb97665c58c92acec91ce80b2d8369358548796
OliverBoliverTheBetter <oliver.reidkonta@education.nsw.gov.au>
2026-05-14 11:02:09 +1000

    may 14th. no survivors in sight

---
+++ PROJECT_DOCUMENTATION.md
@@ -0,0 +1,3 @@
+Prior to the mayeth fourteenth, i had made not only my repository for this fateful task, but also the forms needed to harvest my data for the Assesment Task
+May the fourteenth:
+I have reaped what i sowed, the data fields hath been harvested and now are in the GitHub Repository. The main.py has started its growth. NSWEduChat shant survive.
--- main.py
+++ main.py
@@ -4,13 +4,42 @@
 
 #functions
 def TheMain():
+    
     while True:
         print("yo wassup this is the DATA you gotta believe me\n")
         print("The choices:\n1. View all my data\n2. Visualise all my data\n3. Search for the data\n4. Edit my data\n5. Exit this lovely Juvely program.")
+        
         try:
             thechoicening = int(input("\nYou  must now decide your fate.\n"))
+        
         except:
             print("\nStupid dumb dumb stupid fart face idiot\n")
+        
+        if thechoicening == 1:
+            print("All the data coming up!") #PLEASE FOR THE LOVE OF GOD FUTURE ME HELP
+        
+        elif thechoicening == 2:
+            print('Visualised data coming up!') #PLEASE FOR THE LOVE OF GOD FUTURE ME HELP
+        
+        elif thechoicening == 3:
+            print('Find my data') #PLEASE FOR THE LOVE OF GOD FUTURE ME HELP
+
+        elif thechoicening == 4:
+            print("Whats wrong with my data :(") #PLEASE FOR THE LOVE OF GOD FUTURE ME HELP
+
+        elif thechoicening == 5:
+            print('Do you really want me to die?')
+            break
+
+        else:
+            destiny = input("Dont you feel stupid? dont you feel a little silly? why do you do this to yourself? your typing so you can watch everyone around you die. Think, stephen, think! What will you have after 500 lines?")
+            
+            if destiny == "I'll still have you, tech free agenda":
+                print("Tech free agenda flies of into the sunset, leaving you all alone wiht your thoughts and your laptop")
+            
+            else:
+                print('Die')
+        
         
 
 
